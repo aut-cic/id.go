277c406bb7e9de84f80b1191c15b32e940b06703
Parham Alvani <parham.alvani@gmail.com>
2023-05-21 15:03:43 +0330

    feat: use configs for connection

--- main.go
+++ main.go
@@ -1,33 +1,43 @@
 package main
 
 import (
+	"crypto/tls"
+	"fmt"
 	"log"
 
+	"github.com/aut-cic/id.go/internal/config"
 	"github.com/go-ldap/ldap/v3"
 	"golang.org/x/text/encoding/unicode"
 )
 
 func main() {
-	l, err := ldap.DialURL("ldap://<address>")
+	cfg := config.New()
+
+	l, err := ldap.DialURL(fmt.Sprintf("ldap://%s", cfg.Address))
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer l.Close()
 
-	if err := l.Bind("CN=<username>,CN=Users,DC=aku,DC=ac,DC=ir", "<password>"); err != nil {
+	if err := l.StartTLS(&tls.Config{InsecureSkipVerify: true}); err != nil {
 		log.Fatal(err)
 	}
 
-	userDN := "CN=parham.alvani,CN=Users,DC=aku,DC=ac,DC=ir"
+	if err := l.Bind(fmt.Sprintf("CN=%s,CN=Users,DC=aku,DC=ac,DC=ir", cfg.Username), cfg.Password); err != nil {
+		log.Fatal(err)
+	}
+
+	userDN := "CN=kahr123,CN=Users,DC=aku,DC=ac,DC=ir"
 
 	utf16 := unicode.UTF16(unicode.LittleEndian, unicode.IgnoreBOM)
 	// According to the MS docs in the links above
 	// The password needs to be enclosed in quotes
-	pwdEncoded, _ := utf16.NewEncoder().String("12345")
+	pwdEncoded, _ := utf16.NewEncoder().String("\"Test@123\"")
 	passReq := ldap.NewModifyRequest(userDN, nil)
 
-	passReq.Add("unicodePwd", []string{pwdEncoded})
+	passReq.Replace("unicodePwd", []string{pwdEncoded})
 
 	if err := l.Modify(passReq); err != nil {
+		log.Fatal(err)
 	}
 }
